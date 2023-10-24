9618015f97de0d6745b4e28eeadf73ab765d88d2
0914eagle <67526702+0914eagle@users.noreply.github.com>
2023-10-24 15:25:56 +0900

    Revert "heejae"
    This reverts commit f64c8f388ba3192631bb0e17d07ac9bb3058a87c.

--- iTrust2/src/main/java/edu/ncsu/csc/itrust2/ITrust2Application.java
+++ iTrust2/src/main/java/edu/ncsu/csc/itrust2/ITrust2Application.java
@@ -1,1 +1,11 @@
-package edu.ncsu.csc.itrust2;import org.springframework.boot.SpringApplication;import org.springframework.boot.autoconfigure.SpringBootApplication;@SpringBootApplication(scanBasePackages = {"edu.ncsu.csc.itrust2"})public class ITrust2Application {    public static void main(final String[] args) {        SpringApplication.run(ITrust2Application.class, args);    }}
+package edu.ncsu.csc.itrust2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = {"edu.ncsu.csc.itrust2"})
+public class ITrust2Application {
+    public static void main(final String[] args) {
+        SpringApplication.run(ITrust2Application.class, args);
+    }
+}

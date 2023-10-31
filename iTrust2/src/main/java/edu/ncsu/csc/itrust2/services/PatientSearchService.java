7b219c797e5464422ea1a07af2f51e92ed7cbd40
박정양 <80745010+wjddid000624@users.noreply.github.com>
2023-10-31 18:17:29 +0900

    Delete redundant code

--- iTrust2/src/main/java/edu/ncsu/csc/itrust2/services/PatientSearchService.java
+++ iTrust2/src/main/java/edu/ncsu/csc/itrust2/services/PatientSearchService.java
@@ -6,9 +6,7 @@ import edu.ncsu.csc.itrust2.repositories.PatientRepository;
 import java.util.List;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.web.server.ResponseStatusException;
 
 @RequiredArgsConstructor
 @Service
@@ -17,9 +15,6 @@ public class PatientSearchService {
     private final PatientRepository patientRepository;
 
     public List<Patient> listByPatientName(String nameQuery) {
-        if(nameQuery.length() > 30){
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "patientName is too long");
-        }
         return patientRepository.findAllByFirstNameContainsOrLastNameContainsOrUsernameContains(
                 nameQuery, nameQuery, nameQuery);
     }

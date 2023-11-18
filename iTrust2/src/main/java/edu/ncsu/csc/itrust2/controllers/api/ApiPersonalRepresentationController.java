05814ea0fce4c81f6a1b6dc6e7a601df313e5797
Hajin Kim <kimhajin@yonsei.ac.kr>
2023-11-18 15:13:29 +0000

    apply

--- iTrust2/src/main/java/edu/ncsu/csc/itrust2/controllers/api/ApiPersonalRepresentationController.java
+++ iTrust2/src/main/java/edu/ncsu/csc/itrust2/controllers/api/ApiPersonalRepresentationController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1")
 public class ApiPersonalRepresentationController {
+    private final PersonalRepresentationService personalRepresentationService;
 
 
 

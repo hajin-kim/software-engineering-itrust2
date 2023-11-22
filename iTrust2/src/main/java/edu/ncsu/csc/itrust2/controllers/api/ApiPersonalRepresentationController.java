09cf6c8babd3bb7301a166e2bb6a71bc2a7555bd
qb <grant9390@naver.com>
2023-11-22 12:38:26 +0900

    added annotations for swagger doc

--- iTrust2/src/main/java/edu/ncsu/csc/itrust2/controllers/api/ApiPersonalRepresentationController.java
+++ iTrust2/src/main/java/edu/ncsu/csc/itrust2/controllers/api/ApiPersonalRepresentationController.java
@@ -55,18 +55,25 @@ public class ApiPersonalRepresentationController {
         return personalRepresentationService.listByPatient(patientUsername);
     }
 
+    @Operation(summary = "Patient: 자신의 대리인 지정")
     @PostMapping("/representatives/{representativeUsername}")
     @PreAuthorize("hasRole('ROLE_PATIENT')")
-    public void setPersonalRepresentative(@PathVariable String representativeUsername) {
+    public void setPersonalRepresentative(
+            @Parameter(description = "대리인으로 지정할 환자의 username 입니다.") @PathVariable
+                final String representativeUsername) {
         String currentUsername = loggerUtil.getCurrentUsername();
         personalRepresentationService.setPersonalRepresentation(
                 currentUsername, representativeUsername);
     }
 
+    @Operation(summary = "HCP: 특정 환자의 대리인 지정")
     @PostMapping("/patients/{patientUsername}/representatives/{representativeUsername}")
     @PreAuthorize("hasRole('ROLE_HCP')")
     public void setPersonalRepresentation(
-            @PathVariable String patientUsername, @PathVariable String representativeUsername) {
+            @Parameter(description = "대리인을 지정할 특정 환자의 username 입니다.") @PathVariable
+                final String patientUsername,
+            @Parameter(description = "대리인으로 지정할 환자의 username 입니다.") @PathVariable
+                final String representativeUsername) {
         personalRepresentationService.setPersonalRepresentation(
                 patientUsername, representativeUsername);
     }

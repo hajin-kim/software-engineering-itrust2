b1e9bd852ba2bb545914d6231619658623a67862
Hajin Kim <kimhajin@yonsei.ac.kr>
2023-11-22 13:17:58 +0900

    :memo: Keep convention

--- iTrust2/src/main/java/edu/ncsu/csc/itrust2/controllers/api/ApiEmergencyPatientController.java
+++ iTrust2/src/main/java/edu/ncsu/csc/itrust2/controllers/api/ApiEmergencyPatientController.java
@@ -13,7 +13,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
-@Tag(name = "[UC15] 응급 상황 환자 상세 정보 조회")
+@Tag(name = "[UC15] 응급 상황 환자 상세 정보 조회 API")
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("api/v1/emergency/patients")

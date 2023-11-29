87655d969f6d803ec7af5da171e848da77e64ce3
hunny7434 <55790304+hunny7434@users.noreply.github.com>
2023-11-29 20:58:16 +0900

    이슈 해결
    환자 검색 시 case-sensitive하게 검색 되는 문제, # substring이 정확히 일치하지 않으면 검색이 되지 않는 문제, 이름으로 환자를 검색할 때 First name과 Last name을 합해서 검색하지 못하는 문제 다 해결하였습니다.

--- iTrust2/src/main/java/edu/ncsu/csc/itrust2/services/PatientSearchService.java
+++ iTrust2/src/main/java/edu/ncsu/csc/itrust2/services/PatientSearchService.java
@@ -4,6 +4,7 @@ import edu.ncsu.csc.itrust2.models.Patient;
 import edu.ncsu.csc.itrust2.repositories.PatientRepository;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -14,8 +15,35 @@ public class PatientSearchService {
 
     private final PatientRepository patientRepository;
 
+    private boolean isSubstringOf(String string, String substring) {
+        string = string.toLowerCase();
+        substring = substring.toLowerCase();
+
+        int subStringIndex = 0;
+        int stringIndexT = 0;
+
+        while (subStringIndex < substring.length() && stringIndexT < string.length()) {
+            if (substring.charAt(subStringIndex) == string.charAt(stringIndexT)) {
+                subStringIndex++;
+                stringIndexT++;
+            } else {
+                stringIndexT++;
+            }
+        }
+
+        return subStringIndex == substring.length();
+    }
+
     public List<Patient> listByPatientName(String nameQuery) {
-        return patientRepository.findAllByFirstNameContainsOrLastNameContainsOrUsernameContains(
-                nameQuery, nameQuery, nameQuery);
+        return patientRepository.findAll().stream()
+                .filter(
+                        patient ->
+                                isSubstringOf(
+                                                patient.getFirstName()
+                                                        + " "
+                                                        + patient.getLastName(),
+                                                nameQuery)
+                                        || isSubstringOf(patient.getUsername(), nameQuery))
+                .collect(Collectors.toList());
     }
 }

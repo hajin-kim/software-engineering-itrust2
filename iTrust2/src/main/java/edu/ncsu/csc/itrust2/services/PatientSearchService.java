f1c531c09c608717b3c28a5118486635fb4d3145
Hajin Kim <kimhajin@yonsei.ac.kr>
2023-11-30 03:56:41 +0000

    Apply 1 suggestion(s) to 1 file(s)

--- iTrust2/src/main/java/edu/ncsu/csc/itrust2/services/PatientSearchService.java
+++ iTrust2/src/main/java/edu/ncsu/csc/itrust2/services/PatientSearchService.java
@@ -44,6 +44,6 @@ public class PatientSearchService {
                                                         + patient.getLastName(),
                                                 nameQuery)
                                         || isSubstringOf(patient.getUsername(), nameQuery))
-                .collect(Collectors.toList());
+                .toList();
     }
 }

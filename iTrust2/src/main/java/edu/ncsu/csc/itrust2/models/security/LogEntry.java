c2b1178a5116e45dc795b6234970c69348a45e36
2021147532 <judy01@yonsei.ac.kr>
2023-11-30 14:41:51 +0900

    git pull

--- iTrust2/src/main/java/edu/ncsu/csc/itrust2/models/security/LogEntry.java
+++ iTrust2/src/main/java/edu/ncsu/csc/itrust2/models/security/LogEntry.java
@@ -15,6 +15,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import com.google.gson.annotations.JsonAdapter;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -28,6 +29,8 @@ import lombok.Setter;
  *
  * @author Kai Presler-Marshall
  */
+
+@Schema(description = "이벤트에 관한 로그 기록입니다.")
 @NoArgsConstructor
 @Getter
 @Entity
@@ -35,12 +38,15 @@ import lombok.Setter;
 public class LogEntry extends DomainObject {
 
     /** Type of event that has been logged */
+    @Schema(description = "로그로 기록된 이벤트의 유형입니다.")
     @Setter @NotNull private TransactionType logCode;
 
     /** The primary user for the event that has been logged */
+    @Schema(description = "이벤트의 주요 사용자입니다.")
     @Setter @NotNull private String primaryUser;
 
     /** The timestamp of when the event occurred */
+    @Schema(description = "이벤트 발생 시간입니다.")
     @Setter
     @NotNull @Basic
     // Allows the field to show up nicely in the database
@@ -49,12 +55,15 @@ public class LogEntry extends DomainObject {
     private ZonedDateTime time;
 
     /** The secondary user for the event that has been logged (optional) */
+    @Schema(description = "이벤트의 보조 사용자입니다.")
     private String secondaryUser;
 
     /** An additional elaborative message for the event that has been logged. Optional. */
+    @Schema(description = "로그에 대한 설명 메세지입니다.")
     @Setter private String message;
 
     /** ID of the LogEntry */
+    @Schema(description = "고유 아이디입니다.")
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;

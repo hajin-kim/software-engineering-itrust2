d3ed722bd4069b6293f0a92e6e70942d9395fcc3
박정양 <80745010+wjddid000624@users.noreply.github.com>
2023-12-01 19:52:26 +0900

    Validation test for visualAcuityResult and Sphere of ophthalmologySurgery

--- iTrust2/src/main/java/edu/ncsu/csc/itrust2/models/OphthalmologySurgery.java
+++ iTrust2/src/main/java/edu/ncsu/csc/itrust2/models/OphthalmologySurgery.java
@@ -9,6 +9,10 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.regex.Pattern;
+
+import static edu.ncsu.csc.itrust2.controllers.api.APIController.errorResponse;
+
 @NoArgsConstructor
 @Getter
 @Entity
@@ -20,9 +24,9 @@ public class OphthalmologySurgery extends DomainObject {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-    @NotNull @Setter private Integer leftVisualActuityResult;
+    @NotNull @Setter private Integer leftVisualAcuityResult;
 
-    @NotNull @Setter private Integer rightVisualActuityResult;
+    @NotNull @Setter private Integer rightVisualAcuityResult;
 
     @NotNull @Setter private Float leftSphere;
 
@@ -48,4 +52,53 @@ public class OphthalmologySurgery extends DomainObject {
     @NotNull @ManyToOne
     @JoinColumn(name = "hcp_id", columnDefinition = "varchar(100)")
     private User hcp;
+
+    public boolean checkVisualAcuityResultValid(final Integer visualAcuityResult){
+        if(visualAcuityResult == null){
+            throw new IllegalArgumentException("visual acuity result cannot be null");
+        }
+        if(leftVisualAcuityResult < 20 || leftVisualAcuityResult > 200){
+            throw new IllegalArgumentException("visual acuity result must be the value between 20 and 200.");
+        }
+        return true;
+    }
+
+    public void setLeftVisualAcuityResult(final Integer leftVisualAcuityResult){
+        boolean flag = checkVisualAcuityResultValid( leftVisualAcuityResult );
+        if(flag){
+            this.leftVisualAcuityResult = leftVisualAcuityResult;
+        }
+    }
+
+    public void setRightVisualAcuityResult(final Integer rightVisualAcuityResult){
+        boolean flag = checkVisualAcuityResultValid( rightVisualAcuityResult );
+        if(flag){
+            this.rightVisualAcuityResult = rightVisualAcuityResult;
+        }
+    }
+
+    public boolean checkSphereValid(final Float sphere){
+        if(sphere == null){
+            throw new IllegalArgumentException("sphere cannot be null");
+        }
+        String pattern = "^[-]?\\d+(\\.[0-9]{1,2})?$";
+        if(!Pattern.matches(pattern, String.valueOf(leftSphere))){
+            throw new IllegalArgumentException("sphere must be the floating point number up to two digits.");
+        }
+        return true;
+    }
+
+    public void setLeftSphere(final Float leftSphere){
+        boolean flag = checkSphereValid( leftSphere );
+        if(flag){
+            this.leftSphere = leftSphere;
+        }
+    }
+
+    public void setRightSphere(final Float rightSphere){
+        boolean flag = checkSphereValid( rightSphere );
+        if(flag){
+            this.rightSphere = rightSphere;
+        }
+    }
 }

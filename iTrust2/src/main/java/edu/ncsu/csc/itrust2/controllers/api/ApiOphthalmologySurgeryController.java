9382790278185a023f74f6fdb24e4c6663024e5f
박정양 <80745010+wjddid000624@users.noreply.github.com>
2023-12-02 00:21:39 +0900

    resolve warnings

--- iTrust2/src/main/java/edu/ncsu/csc/itrust2/controllers/api/ApiOphthalmologySurgeryController.java
+++ iTrust2/src/main/java/edu/ncsu/csc/itrust2/controllers/api/ApiOphthalmologySurgeryController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@SuppressWarnings({"unchecked", "rawtypes"})
 public class ApiOphthalmologySurgeryController extends APIController {
 
     private final OfficeVisitService officeVisitService;

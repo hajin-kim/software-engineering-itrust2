def792131b371b3237d10955998e451114f686cb
2021147532 <judy01@yonsei.ac.kr>
2023-12-04 23:04:00 +0900

    fix: isRepresentative

--- iTrust2/src/main/java/edu/ncsu/csc/itrust2/services/PersonalRepresentationService.java
+++ iTrust2/src/main/java/edu/ncsu/csc/itrust2/services/PersonalRepresentationService.java
@@ -8,6 +8,7 @@ import edu.ncsu.csc.itrust2.repositories.PersonalRepresentationRepository;
 import java.util.List;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -61,6 +62,7 @@ public class PersonalRepresentationService {
     public boolean isRepresentative(String currentUsername, String patientName) {
         final Patient currentUser = (Patient) patientService.findByName(currentUsername);
         final Patient patient = (Patient) patientService.findByName(patientName);
-        return personalRepresentationRepository.isRepresentative(patient, currentUser);
+        return personalRepresentationRepository.existByPatientAndPersonalRepresentative(
+                patient, currentUser);
     }
 }
